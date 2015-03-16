bdb6a78a781a8a1cf1c602d2d81aa663a0bae0e7
Connor Grady <connor.grady@gradycapital.us>
2015-03-15 22:12:30 -0500

    Finished Set class

--- src/Set.java
+++ src/Set.java
@@ -26,11 +26,6 @@ public class Set <E extends Comparable<E>> {
     private int size = 0;
 
     /**
-     * Define capacity variable and initialize to 0
-     */
-    private int capacity = 0;
-
-    /**
      * Define underlying generic array
      */
     private E[] data;
@@ -39,7 +34,10 @@ public class Set <E extends Comparable<E>> {
      * Basic Constructor to create new Set
      */
     public Set() {
-        capacity = INITIAL_CAPACITY;
+     /**
+      * Define capacity variable and initialize to INITIAL_CAPACITY
+      */
+        int capacity = INITIAL_CAPACITY;
         this.data = (E[]) new Comparable[capacity];
     }
 
@@ -48,6 +46,7 @@ public class Set <E extends Comparable<E>> {
      * @param n Desired name of new Set as String
      */
     public Set(String n) {
+        // Initialize name to the specified name
         this.name = n;
     }
 
@@ -56,6 +55,7 @@ public class Set <E extends Comparable<E>> {
      * @return Name of Set as String
      */
     public String getName() {
+        // Return the value of name
         return this.name;
     }
 
@@ -64,6 +64,7 @@ public class Set <E extends Comparable<E>> {
      * @param n Desired name of Set as String
      */
     public void setName(String n) {
+        // Initialize name to the specified name
         this.name = n;
     }
 
@@ -113,10 +114,12 @@ public class Set <E extends Comparable<E>> {
      * @return The object at the specified index
      */
     public E get(int i) {
+        // Make sure the index is not out of bounds
         if (i < size) {
+            // Return the element at the specified index
             return (E) data[i];
         } else {
-            throw new NoSuchElementException();
+            throw new IndexOutOfBoundsException();
         }
     }
 
@@ -126,20 +129,32 @@ public class Set <E extends Comparable<E>> {
      * @return The object that was removed
      */
     public E remove(E item) {
+        // Create and initialize to null a generic object
         E obj = null;
+        // Iterate over data
         for (int i = 0; i < data.length; i++) {
+            // Check if the current element matches the specified item
             if (data[i].equals(item)) {
+                // Set obj to the current element
                 obj = data[i];
+                // Set the current element to null
                 data[i] = null;
+                // Declare tmp to the current index
                 int tmp = i;
+                // Loop to move elements left
                 while (tmp < size) {
+                    // Element at current tmp is replaced by element at tmp + 1
                     data[tmp] = data[tmp+1];
+                    // Nullify element at tmp + 1
                     data[tmp+1] = null;
+                    // Step tmp by one
                     tmp++;
                 }
+                // Drop size by one
                 size--;
             }
         }
+        // If obj is not null, return it
         if (obj != null) {
             return obj;
         } else {
@@ -155,8 +170,20 @@ public class Set <E extends Comparable<E>> {
     public E remove(int index) {
         E obj = data[index];
         data[index] = null;
-        // Need to rewrite to shift objects over
-        // Can't leave empty object in array
+        // Declare tmp to the current index
+        int tmp = index;
+        // Loop to move elements left
+        while (tmp < size) {
+            // Element at current tmp is replaced by element at tmp + 1
+            data[tmp] = data[tmp+1];
+            // Nullify element at tmp + 1
+            data[tmp+1] = null;
+            // Step tmp by one
+            tmp++;
+        }
+        // Drop size by one
+        size--;
+        // Return the removed object if it's not null
         if (obj != null) {
             return obj;
         } else {
@@ -171,12 +198,14 @@ public class Set <E extends Comparable<E>> {
      */
     public int indexOf(E target) {
         if (target == null) {
+            // This allows us to handle null objects
             for (int i = 0; i < size; i++) {
                 if (this.data[i] == null) {
                     return i;
                 }
             }
         } else {
+            // Find the matching object and return its index (value of current i)
             for (int i = 0; i < size; i++) {
                 if (target.equals(this.data[i])) {
                     return i;
@@ -184,6 +213,65 @@ public class Set <E extends Comparable<E>> {
             }
         }
         return -1;
+    }
+
+    /**
+     * Return the union of the current Set and the specified Set
+     * @param s The Set to be unified with the current Set
+     * @return the union of the current Set and the specified Set
+     */
+    public Set<E> union(Set<E> s) {
+        for (int i = 0; i < size; i++) {
+            s.add(data[i]);
+        }
+        return s;
+    }
+
+    /**
+     * Return a set of the common elements between the current Set and the specified Set
+     * @param s The Set to be intersected with the current Set
+     * @return the intersection of the current Set and the specified Set
+     */
+    public Set<E> intersect(Set<E> s) {
+        for (int i = 0; i < size; i++) {
+            if (!data[i].equals(s.get(i))) {
+                s.remove(i);
+            }
+        }
+        return s;
+    }
+
+    /**
+     * Remove all objects from the current Set that are in the specified Set
+     * @param s The Set to be subtracted from the current Set
+     * @return the set with all objects in the specified Set removed
+     * This seems, to me at least, the same as difference. Is that right?
+     */
+    public Set<E> subtract(Set<E> s) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(s.get(i))) {
+                s.remove(i);
+            } else {
+                s.add(data[i]);
+            }
+        }
+        return s;
+    }
+
+    /**
+     * Return a set of the uncommon elements between the current Set and the specified Set
+     * @param s The Set to be differentiated with the current Set
+     * @return the difference of the current Set and specified Set
+     */
+    public Set<E> difference(Set<E> s) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(s.get(i))) {
+                s.remove(i);
+            } else {
+                s.add(data[i]);
+            }
+        }
+        return s;
     }
 
     /**
@@ -195,6 +283,7 @@ public class Set <E extends Comparable<E>> {
         if (size != s.size()) {
             return false;
         } else {
+            // For the element at the specified index in each array, determine if they equal
             for (int i = 0; i < size; i++) {
                 if (data[i] != s.get(i)) {
                     return false;
@@ -205,10 +294,19 @@ public class Set <E extends Comparable<E>> {
     }
 
     /**
+     * Clear all the elements in the Set
+     */
+    public void clear() {
+        // Replace each element in data with null
+        Arrays.fill(data, null);
+    }
+
+    /**
      * Get size of the Set
      * @return Number of elements in Set
      */
     public int size() {
+        // Return the length of the underlying data array
         return this.data.length;
     }
 
@@ -218,13 +316,19 @@ public class Set <E extends Comparable<E>> {
      */
     @Override
     public String toString() {
+        // Create StringBuilder object
         StringBuilder s = new StringBuilder();
+        // Append the Set's name followed by ":{"
         s.append(this.name).append(":{");
+        // For each element in the Set, append it the StringBuilder followed by ","
         for (E obj : this.data) {
             s.append(obj).append(",");
         }
+        // Delete the trailing character (will be a ",")
         s.deleteCharAt(s.length() -1);
+        // Append the closing "}"
         s.append("}");
+        // Return the String value of the StringBuilder
         return String.valueOf(s);
     }
 }
